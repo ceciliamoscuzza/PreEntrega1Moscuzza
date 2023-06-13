54e25359fd5632e476873f3c7b73cebe409ed91d
Cecilia Moscuzza <cecimoscu@gmail.com>
2023-06-13 10:18:01 -0300

    hice el primer condicional en js

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,17 @@
-prompt;
+let usuario = prompt("Ingrese su Nombre y apellido:");
+
+let edad = prompt("Ingrese su edad:");
+
+if (edad >= 21) {
+  alert("🥃👋🏼Bienvenidx " + usuario);
+} else {
+  alert("🚫No podés sumarte a esta experiencia, tenés que ser mayor de edad🚫");
+}
+
+let experiencia = prompt(
+  "Elegí tu CataExperience🥃: Nacional, Importado, De autor; Mix Premium"
+);
+let nacional;
+let importado;
+let deAutor;
+let mixPremium;
