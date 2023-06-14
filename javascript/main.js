2e119c5e71933e5951cc10beda3071bdd43a11fa
Cecilia Moscuzza <cecimoscu@gmail.com>
2023-06-14 14:53:05 -0300

    arme swicht y comence for

--- javascript/main.js
+++ javascript/main.js
@@ -9,8 +9,47 @@ if (edad >= 21) {
   let experiencia = prompt(
     "Elegí tu CataExperience🥃: a) Nacional , b) Importado, c) De autor; d) Mix Premium"
   );
+  mostrarExperiencia(experiencia);
+  let mensaje = mostrarDescuentos();
+  alert(mensaje);
 } else {
   alert(
     "🚫No podemos sumarte a esta experiencia, tenés que ser mayor de edad🚫"
   );
+}
+function mostrarExperiencia(cata) {
+  switch (cata.toLowerCase()) {
+    case "a":
+      alert(
+        "La experiencia Nacional incluye la cata de 4 variedades. Su precio es de $5000"
+      );
+      break;
+    case "b":
+      alert(
+        "La experiencia Importado incluye la cata de 4 variedades. Su precio es de $8000"
+      );
+      break;
+    case "c":
+      alert(
+        "La experiencia De autor incluye la cata de 5 variedades. Su precio es de $12000"
+      );
+      break;
+    case "d":
+      alert(
+        "La experiencia Mix Premium incluye la cata de 6 variedades. Su precio es de $16000"
+      );
+      break;
+    default:
+      alert("por favor ingresá una opción válida");
+      break;
+  }
+}
+
+function mostrarDescuentos() {
+  let mensaje = "";
+  for (let i = 2; i <= 10; i++) {
+    let descuento = i * 5;
+    mensaje += i + "  personas " + descuento + "% de descuento\n";
+  }
+  return mensaje;
 }
